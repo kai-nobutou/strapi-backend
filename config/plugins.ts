2339fa6cefc5d7d399a426de7881d5c70654f491
Kai Nobuto <nobutokai0624@icloud.com>
2025-02-25 20:08:07 +0900

    4

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    'content-type-builder': {
+      enabled: true,
+      config: {
+        plugin: {
+          // 開発モード以外でもContent-Type Builderを有効化
+          development: true,
+          production: true,
+        },
+      },
+    },
+  });
