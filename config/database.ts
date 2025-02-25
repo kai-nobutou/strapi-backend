3402fc1b2cc53f4e1f94fdf54c15bc019aa840aa
Kai Nobuto <nobutokai0624@icloud.com>
2025-02-25 17:38:44 +0900

    2

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false }, // SSL必須
+      ssl: false, // SSLを無効にする
     },
     pool: { min: 2, max: 10 },
   },
